c4b13b19de8dc3d2e739f4be73d480286f15471a
Alina Leuca <alinaleuca@gmail.com>
2017-10-11 20:03:58 +0200

    added bouncing ball

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,13 @@
 //=========================
 //Setup & draw functions
 //=========================
+
+var x = 100
+var y = 100
+var radiusX = 10
+var radiusY = 10
+var speed = 5
+
 function setup() {
     makeCanvas()
 }
@@ -15,20 +22,32 @@ function windowResized() {
 }
 
 function draw() {
-    clear()
-    background("rgba(255, 255, 255, 1)")
+    // clear()
+    // background("rgba(255, 255, 255, 1)")
     update()
 }
 
 function update() {
     stroke("rgba(50, 50, 50, 0.8)")
     fill("rgba(50, 50, 50, 0.8)")
-    ellipse(100, 100, 10, 10)
+    if (x >= windowWidth) {
+        speed = -5
+    }
+    if (x <= 0) {
+        speed = 5
+    }
+    x = x + speed
+    ellipse(x, y, radiusX, radiusY)
+    // if x reaches windowwidth then the ellipse starts subtracting 5
 }
+
+
+
 
 //=========================
 //Interactivity functions
 //=========================
 function touchStarted() {
+    clear()
     console.log("Touch!", mouseX, mouseY)
 };
